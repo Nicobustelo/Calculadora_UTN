209dd1232d94c08bebda04c30604c2c475689ed7
Nico Bustelo <nbustelo2002@gmail.com>
2022-09-15 21:14:54 -0300

    mejoras interfaz
    calculadora codigo binario, inverso y complemento

--- bases.js
+++ bases.js
@@ -4,3 +4,60 @@ const countries = {
 	'es-ES': 'Octal',
 	'hr-HR': 'Hexadecimal',
 };
+
+// function embedCalculatorWidget() {
+// 	function k(a) {
+// 		a.origin == c.replace('//', '//embed.') &&
+// 			((a = JSON.parse(a.data)),
+// 			a.height && e == a.id && f == a.code && (d.height = a.height));
+// 	}
+// 	var b = document.getElementsByTagName('script');
+// 	b = b[b.length - 1];
+// 	b = b.parentNode;
+// 	b.style.textAlign = 'right';
+// 	var a = b.getElementsByTagName('a')[0],
+// 		f = a.getAttribute('data-code'),
+// 		l = a.getAttribute('data-lang'),
+// 		m = a.getAttribute('data-v'),
+// 		n = a.getAttribute('data-colors').replace(/#/g, ''),
+// 		c = a.href,
+// 		g = c.lastIndexOf('/'),
+// 		h = c.lastIndexOf('/', g - 1),
+// 		e = c.substring(h + 1, g);
+// 	c = c.substring(0, h);
+// 	a = a.style;
+// 	a.textDecoration = 'none';
+// 	a.color = 'black';
+// 	a.backgroundImage = "url('" + c + "/img/lic/by-sa_s.png')";
+// 	a.backgroundPosition = 'left,center';
+// 	a.backgroundRepeat = 'no-repeat';
+// 	a.paddingLeft = '44px';
+// 	a.paddingTop = '10px';
+// 	a.paddingBottom = '10px';
+// 	a.paddingRight = '20px';
+// 	a.fontFamily = 'Roboto,sans-serif';
+// 	a.fontSize = '12px';
+// 	a.display = 'inline-block';
+// 	a.boxShadow = 'none';
+// 	window.addEventListener && window.addEventListener('message', k);
+// 	var d = document.createElement('iframe');
+// 	d.src =
+// 		c.replace('//', '//embed.') +
+// 		'/embed3/?id=' +
+// 		e +
+// 		'&_d=' +
+// 		f +
+// 		'&locale=' +
+// 		l +
+// 		'&colors=' +
+// 		n +
+// 		'&v=' +
+// 		m;
+// 	d.style.borderWidth = 0;
+// 	d.style.border-radius = '10px';
+// 	d.style.width = '100%';
+// 	d.style.marginBottom = '0';
+// d.style.background = '#fff';
+// 	b.insertBefore(d, b.firstChild);
+// }
+// embedCalculatorWidget();
